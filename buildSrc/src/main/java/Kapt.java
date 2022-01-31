8e68c84e7ea69badb6a5eb22e724687a069601ef
aleh-god <aleh.god@gmail.com>
2022-01-31 23:00:04 +0300

    add: dependencies names class

---
+++ buildSrc/src/main/java/Kapt.java
@@ -0,0 +1,10 @@
+public class Kapt {
+    // Annotation processor
+
+    // androidx.lifecycle
+    static String lifecycleCompiler = "androidx.lifecycle:lifecycle-compiler:" + Versions.ktx_version;
+    // androidx.room
+    static String roomCompiler = "androidx.room:room-compiler:" + Versions.room_version;
+    //dagger
+    static String daggerCompiler = "com.google.dagger:dagger-compiler:" + Versions.dagger_version;
+}
